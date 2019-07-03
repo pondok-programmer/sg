ca35180cfeb6c2ccaa145a85b67bdda866f179f4
PondokiOS (Muhammmad Muizzsuddin) <muiz.idn@gmail.com>
2019-07-03 09:16:58 +0700

    First successfull build

--- Package.swift
+++ Package.swift
@@ -5,16 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "sg",
+    platforms: [
+        .macOS(.v10_11)
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/muizidn/SwiftGit2", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "sg",
-            dependencies: []),
+            dependencies: ["SgLib"]),
+        .target(
+            name: "SgLib",
+            dependencies: ["SwiftGit2"]),
         .testTarget(
             name: "sgTests",
             dependencies: ["sg"]),
--- Sources/sg/main.swift
+++ Sources/sg/main.swift
@@ -1,1 +1,3 @@
+import SgLib
+
 print("Hello, world!")
